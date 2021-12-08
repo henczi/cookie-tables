a14e9fa5689cc485dd1f897da1df8f8433a1d776
Henczidai Bálint <henczidaibalint@gmail.com>
2021-12-08 19:09:56 +0100

    init

---
+++ src/lib/Counter.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import Button from "./Button.svelte";
+
+
+  let count: number = 0
+  const increment = () => {
+    count += 1
+  }
+</script>
+
+<Button on:click={increment}>
+  Clicks: {count}
+</Button>
---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { dirname, join } from 'path'
+import { fileURLToPath } from 'url'
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+
+const _dirname = dirname(fileURLToPath(import.meta.url))
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [svelte()],
+  resolve: {
+    alias: {
+      $lib: join(_dirname, "src/lib"),
+      $assets: join(_dirname, "src/assets"),
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        popup: join(_dirname, 'popup.html'),
+        options: join(_dirname, 'options.html'),
+      },
+    }
+  }
+})
